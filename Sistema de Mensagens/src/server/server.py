cc752be6a504ddc20de2f820c2732755df24f7f1
Diego Correa <diegocorrea.cc@gmail.com>
2017-11-22 17:37:22 -0300

    adicionando package e preparando usuario no servidor

--- Sistema de Mensagens/src/server/server.py
+++ Sistema de Mensagens/src/server/server.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import rpyc
 import sys
-from .user import User
 sys.path.append('..')
+from user import User
 
 userList = { }
 groupList = { }
 
 class ServerService(rpyc.Service):
+    userConected = None
     def on_connect(self):
         # code that runs when a connection is created
         # (to init the serivce, if needed)
@@ -17,25 +18,56 @@ class ServerService(rpyc.Service):
         # code that runs when the connection has already closed
         # (to finalize the service, if needed)
         pass
+    #
+    # CREATORS
+    #
     @classmethod # this is an exposed method
-    def createUser(cls): 
+    def createUser(cls, name): 
+        newUser = User(name)
+        userList.setdefault(newUser.getId(), newUser)
+        return newUser
+    @classmethod # this is an exposed method
+    def createGroup(cls, name):
+        pass
+    #
+    # REMOVERS
+    #
+    @classmethod # this is an exposed method
+    def deleteUser(cls, id):
         pass
     @classmethod # this is an exposed method
-    def createGroup(cls):
+    def deleteGroup(cls, id):
+        pass
+    #
+    # SENDERS
+    #
+    @classmethod # this is an exposed method
+    def sendMessageUser(cls, id, message):
         pass
     @classmethod # this is an exposed method
-    def deleteUser(cls):
+    def sendMessageGroup(cls, id, message):
+        pass
+    #
+    # FINDERS
+    #
+    @classmethod # this is an exposed method
+    def findUserByName(cls, name):
         pass
     @classmethod # this is an exposed method
-    def deleteGroup(cls):
+    def findGroupByName(cls, name):
         pass
     @classmethod # this is an exposed method
-    def sendMessageUser(cls):
+    def findUserById(cls, id):
         pass
     @classmethod # this is an exposed method
-    def sendMessageGroup(cls):
+    def findGroupById(cls, id):
         pass
-
+    #
+    # ALL
+    #
+    @classmethod # this is an exposed method
+    def allGroupList(cls, id):
+        pass
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
     t = ThreadedServer(ServerService, port=27000)
--- Sistema de Mensagens/src/server/user.py
+++ Sistema de Mensagens/src/server/user.py
@@ -1,13 +1,17 @@
+import uuid
+
 class User:
     name = ''
-    id = 0
+    id = ''
     groupList = []
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
+        self.id = uuid.uuid1()
         self.name = name
         pass
     def getId(self):
         return self.id
+    def getName(self)
+        return self.name
     def getGroupList(self):
         return self.groupList
     def addGroup(self, newGroup):
